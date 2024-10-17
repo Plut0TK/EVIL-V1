a15c0b332921d9e856141654f10a94774d7d560d
Plut0TK <efekeere5@gmail.com>
2024-10-17 23:43:43 +0100

    Update XeonBug5.js

--- XeonBug5.js
+++ XeonBug5.js
@@ -1,11 +1,11 @@
-//base by Tech-God
+//base by Pluto
 //re-upload? recode? copy code? give credit ya :)
-//YouTube: @techgod143
-//Instagram: techgod143
-//Telegram: t.me/techgod143
-//GitHub: @techgod143
-//WhatsApp: +917466008456
-//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@techgod143
+//YouTube: @MrBeast
+//Instagram: MrBeast
+//Telegram: t.me/Pluto
+//GitHub: @Plut0TK
+//WhatsApp: +2347036314248
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@MrBeast
 
 const { default: makeWaSocket, useMultiFileAuthState, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require('@whiskeysockets/baileys')
 const os = require('os')
